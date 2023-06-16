7ff8740d0178e57f13b0dce872004f9c819c1a90
Jedson Azevedo <jedazevedo@gmail.com>
2023-06-16 03:12:33 -0300

    criando app

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+# HTTP REQUEST
+def my_view(request):
+    return HttpResponse("TESTING")
+def about(request):
+    return HttpResponse('main view')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/',my_view),
+    path('', about),
 ]
