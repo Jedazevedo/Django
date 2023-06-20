bb957615b3a5698542fb14dac20f4cd3f9e1c294
Jedson Azevedo <jedazevedo@gmail.com>
2023-06-20 01:50:13 -0300

    poucos pacotes de entrada e de html

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, template_name='Home.html')  
+    return render(request, template_name='Home.html', status='201')  
 
 def about(request):
     return HttpResponse('<h1>Sobre</h1>')
